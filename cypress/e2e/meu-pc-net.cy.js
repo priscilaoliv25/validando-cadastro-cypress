97a8a11d2b92bb8ef2c568336fde0726e5425b9b
priscilaoliveirasilva <priscila.coliveira8@senacsp.edu.br>
2023-08-14 22:30:46 -0300

    validando login

--- cypress/e2e/meu-pc-net.cy.js
+++ cypress/e2e/meu-pc-net.cy.js
@@ -1,6 +1,7 @@
+require('@cypress/xpath');
 describe('Testando Meu PC.net', () => {
   it('Validando cadastro', () => {
-    
+
     // Entrando no website escolhido, no caso meupc.net
     cy.visit('https://meupc.net/')
 
@@ -12,10 +13,10 @@ describe('Testando Meu PC.net', () => {
 
     // Clicando no botão de cadastro pelo 
     cy
-    .get('ul.menu-list') // Pegando a ul com a classe .menu-list
-    .children('li') // Pegando os filhos da ul
-    .children('a[href="https://meupc.net/cadastro"]') // Pegando o filho da li que tem o href com valor https://meupc.net/cadastro  
-    .click() // Clicar
+      .get('ul.menu-list') // Pegando a ul com a classe .menu-list
+      .children('li') // Pegando os filhos da ul
+      .children('a[href="https://meupc.net/cadastro"]') // Pegando o filho da li que tem o href com valor https://meupc.net/cadastro  
+      .click() // Clicar
 
     // Preenchendo o campo nome do cadastro com o valor teste
     cy.get('input[name="nome"]').type('teste')
@@ -27,11 +28,11 @@ describe('Testando Meu PC.net', () => {
     cy.get('input[placeholder="Defina uma senha"]').type('abc1234')
 
     // Uma forma de pegar um elemento pelo texto
-   //  cy.contains('Li e concordo com').click()
+    //  cy.contains('Li e concordo com').click()
 
-     //Clicando no checkbox de aceitar os termos 
-     //check para marcar e uncheck para desmarcar
-    cy.get('input[type="checkbox"]').check({force:true})
+    //Clicando no checkbox de aceitar os termos 
+    //check para marcar e uncheck para desmarcar
+    cy.get('input[type="checkbox"]').check({ force: true })
 
     //Clicando no botão de cadastrar
     cy.contains('Cadastrar-se').click()
@@ -39,4 +40,26 @@ describe('Testando Meu PC.net', () => {
     //Verificando se chegamos na tela final do cadastro
     cy.contains('Escolha seu nome de usuário').should('be.visible');
   })
+
+  it.only('Validando login', () => {
+    /* Login: testecypress@tuamaeaquelaursa.com
+    Senha: testecypress
+
+    VERIFICAR SE A TELA POSSUI O TEXTO "PC atual de TesteCypress"
+    */
+
+    cy.visit('https://meupc.net');
+    cy.wait(1000);
+    cy.get('.navbar-burger').click();
+    cy.get('ul.menu-list').children('li').children('a[href="https://meupc.net/login"]').click()
+
+    cy.contains('Email ou nome de usuário').siblings('input').type('testecypress@tuamaeaquelaursa.com');
+    cy.xpath('/html/body/main/section/div/div/form/section/div[4]/span/input').type('testecypress'); // Pelo Xpath
+    //cy.contains('Senha').siblings('span').children('input').type('testecypress') // Pelo Filho e irmão
+    cy.xpath('/html/body/main/section/div/div/form/section/div[5]/button[2]').click(); //Pelo Xpath
+    //cy.contains('Cancelar').siblings('button').click(); // Pelo Irmão
+    cy.contains('PC atual de TesteCypress').should('be.visible');
+  })
+
+
 })
